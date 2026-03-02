a45884a583e0dc1d274c5e296adc9143d7d3d071
Jacob Lo <Jacob Lo@JACOBNB>
2026-03-02 13:45:57 +0800

    加入專案檔案。

---
+++ Training.API/Program.cs
@@ -0,0 +1,32 @@
+using Training.Core;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<SystemInfo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/system-info", (SystemInfo systemInfo) =>
+{
+    return Results.Ok(systemInfo.GetOSAndDateTime());
+})
+.WithName("GetSystemInfo")
+.WithOpenApi();
+
+app.Run();
+
---
+++ Training.Core/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Training.Core
+{
+
+    public class SystemInfo
+    {
+        public string GetOSAndDateTime()
+        {
+
+            string osName = Environment.OSVersion.VersionString;
+
+            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            return $"作業系統: {osName}, 時間: {currentTime} 測試";
+        }
+
+    }
+
+}
